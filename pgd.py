fix: fix gradient in pgd pipeline

pgd.py
@@ -98,17 +98,9 @@ def train(opt):
                 im_mask = F.pad(im_mask, p2d, "constant", 0)
 
                 adv_im = im * (1 - im_mask) + im_mask * pad_patch
-                adv_im = adv_im.unsqueeze(dim=0)
                 
-                loss, _ = compute_loss(adv_im)
-                print(loss)
-
-                # gre = grayscale_cam.reshape((384,640,1)).repeat(1,1,3) * 255
-                # gre = gre.detach().cpu().numpy()
-                # Image.fromarray(gre.astype('uint8')).save("gray.png")
-
-                # grayscale_cam = cam(input_tensor=adv_im, targets=targets)
-                # grayscale_cam = grayscale_cam[0, :]
+                pred = yolo(adv_im)
+                loss, _ = compute_loss(pred)
 
                 grad_ = torch.autograd.grad(loss, noise,
                                             retain_graph=False, create_graph=False)[0]
@@ -122,7 +114,7 @@ def train(opt):
             noise = torch.clamp(noise, min=0, max=1)
 
         
-        tensor2img(noise, f"./submission/pgd_small/pgd_attention_epoch{epoch}.png")
+        tensor2img(noise, f"./submission/pgd_small/pgd_small_epoch{epoch}.png")
         tensor2img(mask, f"./submission/pgd_small/mask.png")
 
 

util/loss.py
@@ -79,6 +79,7 @@ class Original_loss_gpu:
         for i, pi in enumerate(p):  # layer index, layer predictions
             # image, anchor, gridy, gridx
 
+            bs = pi.shape[0]  # batch size
             pi = pi.view(-1, pi.shape[-1])
             best_class = pi[...,5:].max(dim=1).values
             mask = torch.zeros(pi.shape[0],device=self.device)
@@ -89,7 +90,12 @@ class Original_loss_gpu:
             conf = torch.sigmoid(pi[...,4])
             lobj += (mask*conf).sum()
             cnt += float(mask.sum())
-        return lobj * 100, cnt
+        
+        lobj *= 10
+        print("cnt: ", cnt)
+        print("lobj: ", lobj)
+
+        return lobj * bs, cnt
 
 class Faster_RCNN_loss:
     def __init__(self):